69f8ce41ffb5d206f0bcf8cabc8eff58c889485c
iTim-iMac <>
2018-08-28 21:47:20 +0700

    install jwtHelperService

--- MeetingDoc-SPA/src/app/_services/auth.service.ts
+++ MeetingDoc-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHtlper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
@@ -16,6 +19,7 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user);
+          this.decodedToken = this.jwtHtlper.decodeToken(user.token);
         }
       })
     );
@@ -25,5 +29,14 @@ export class AuthService {
     return this.http
       .post(this.baseUrl + 'forgetPassword', email)
       .pipe(map((response: any) => {}));
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHtlper.isTokenExpired(token);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
   }
 }
